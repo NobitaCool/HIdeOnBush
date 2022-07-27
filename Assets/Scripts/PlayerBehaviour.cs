60bc0b78f2f5d852e17a04f03ea455b12bb18a8e
Đặng Vũ Quang Minh <dvqminh98@gmail.com>
2022-07-27 21:29:24 +0700

    Adding player sample background
    Player movement, collision handle for player

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,24 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private BoxCollider2D boxCollider2D;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+    public Joystick joystick;
+
+    private void Start()
     {
-        //Gia Bảo đã tới đây 
-
-
-       // Player Movement
-
-        //Gia Bảo đã tới đây
-        /// duythu
-
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
-
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float x = joystick.Horizontal;
+        float y = joystick.Vertical;
+
+        //reset MoveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        //Swap sprite direction when going left or right
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider2D.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(0,moveDelta.y*Time.deltaTime,0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider2D.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x*Time.deltaTime, 0, 0);
+        }
+
     }
 }
