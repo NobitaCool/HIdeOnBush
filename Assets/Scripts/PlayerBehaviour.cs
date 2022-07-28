f11088fa37e5f032b979ed579c84673544d19bec
Đặng Vũ Quang Minh <dvqminh98@gmail.com>
2022-07-28 09:46:17 +0700

    Player change
    Make player change into tree when not moving

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,10 +9,16 @@ public class PlayerBehaviour : MonoBehaviour
     private Vector3 moveDelta;
     private RaycastHit2D hit;
     public Joystick joystick;
-
+    private SpriteRenderer playerRender;
+    private Sprite playerSprite, treeSprite;
     private void Start()
     {
         boxCollider2D = GetComponent<BoxCollider2D>();
+        playerRender = GetComponent<SpriteRenderer>();
+
+        playerSprite = Resources.Load<Sprite>("Player");
+        treeSprite = Resources.Load<Sprite>("TreeSprite");
+        playerRender.sprite = playerSprite;
     }
     // Update is called once per frame
     private void FixedUpdate()
@@ -24,14 +30,24 @@ public class PlayerBehaviour : MonoBehaviour
         moveDelta = new Vector3(x, y, 0);
 
         //Swap sprite direction when going left or right
+        //Swap sprite render when stay still or moving
         if (moveDelta.x > 0)
         {
+            playerRender.sprite = playerSprite;
             transform.localScale = Vector3.one;
-        }else if (moveDelta.x < 0)
+        }
+        else if (moveDelta.x < 0)
         {
+            playerRender.sprite = playerSprite;
             transform.localScale = new Vector3(-1, 1, 1);
         }
+        else if(moveDelta.x==0)
+        {
+            playerRender.sprite = treeSprite;
+        }
 
+
+        //Draw racycast to check for collider
         hit = Physics2D.BoxCast(transform.position, boxCollider2D.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
         if (hit.collider == null)
         {
