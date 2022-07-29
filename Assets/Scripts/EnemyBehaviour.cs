5a058a339d645c13ecd12608f47a8b0b965f0487
Đặng Vũ Quang Minh <dvqminh98@gmail.com>
2022-07-29 15:06:34 +0700

    Level 1 Design

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -20,9 +20,15 @@ public class EnemyBehaviour : MonoBehaviour
             [SerializeField] private int curIndex = 0;
     #endregion
     #endregion
-    private void OnValidate() => navMesh = GetComponent<NavMeshAgent>();
+    private void OnValidate()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.updateRotation = false;
+        navMesh.updateUpAxis = false;
+    }
 
-    private void Update()
+
+        private void Update()
     {
         if (!isChasing) Patrolling();
 
